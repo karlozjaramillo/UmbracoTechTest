3eab73c0ffb57b61c45b7446bf014b3abb0e5c8d
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2021-10-05 11:20:39 -0500

    Se adapta landing News para que funcione con MVC

---
+++ UmbracoTechTest/Controllers/NewsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoTechTest.Controllers
+{
+    public class NewsController : SurfaceController, IRenderMvcController
+    {
+        public ActionResult Index(RenderModel model)
+        {
+            return View(model.Content);
+        }
+    }
+}
